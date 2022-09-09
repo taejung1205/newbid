2f86026cce78858364134bd6cb876f764603e872
Taejung Kim <immcoc1@gmail.com>
2022-09-09 21:07:13 +0900

    feature: about page bottom box

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,3 +1,5 @@
+import { useScrollIntoView } from "@mantine/hooks";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { Marquee } from "~/components/Animated";
 import { Space } from "~/components/Space";
@@ -5,10 +7,8 @@ import { Space } from "~/components/Space";
 const AboutPageBox = styled.div`
   overflow: hidden;
   width: inherit;
-  height: 100vh;
   background-color: #152dff;
-  display: flex;
-  flex-direction: column;
+  position: relative;
 `;
 
 const Divider = styled.div`
@@ -26,37 +26,134 @@ const MarqueeText = styled.div`
   color: #cccccc;
 `;
 
+interface BoxProps {
+  isScrolledDown: boolean;
+}
+
+const MarqueeBox = styled.div<BoxProps>`
+  transition: all ease-out 1s;
+`;
+
+const BottomBox = styled.div<BoxProps>`
+  transition: all ease-out 1s;
+  height: 100vh;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const BottomTitleText = styled.text`
+  font-weight: 900;
+  color: white;
+  font-size: 20px;
+  line-height: 27px;
+  display: block;
+`;
+
+const BottomBodyText = styled.text`
+  color: white;
+  font-size: 16px;
+  line-height: 18px;
+  display: block;
+`;
+
 export default function Index() {
+  let prevScrollY = useRef(0);
+  const topScrollY = useScrollIntoView<HTMLDivElement>({
+    duration: 3000,
+    onScrollFinish: () => {
+      setIsScrolledDown(false);
+    },
+  });
+  const bottomScrollY = useScrollIntoView<HTMLDivElement>({
+    duration: 3000,
+    onScrollFinish: () => {
+      setIsScrolledDown(true);
+    },
+  });
+  const [isScrolledDown, setIsScrolledDown] = useState<boolean>(false);
+
+  useEffect(() => {
+
+    function handleScroll(event: any) {
+      const currentScrollY = window.scrollY;
+      if (prevScrollY.current > currentScrollY && isScrolledDown && currentScrollY < 500) {
+        topScrollY.scrollIntoView();
+      }
+      if (prevScrollY.current < currentScrollY && !isScrolledDown && currentScrollY > 150) {
+        bottomScrollY.scrollIntoView();
+      }
+      prevScrollY.current = currentScrollY;
+      console.log(isScrolledDown, currentScrollY);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isScrolledDown]);
+
   return (
     <AboutPageBox>
+      <div ref={topScrollY.targetRef} />
       <Space height={140} />
-      <Divider />
-      <Marquee duration={10}>
-        <MarqueeText>예술 시장은 성행하고 있지만 왜</MarqueeText>
-      </Marquee>
-      <Divider />
-      <Marquee duration={20} isReverse>
-        <MarqueeText>
-          아트 작품을 소유하고 컬렉팅하는 장벽은 여전히 높기만 하다
-        </MarqueeText>
-      </Marquee>
-      <Divider />
-      <Marquee duration={10}>
-        <MarqueeText>예술 시장에 대한 의문점 의문점</MarqueeText>
-      </Marquee>
-      <Divider />
-      <Marquee duration={10} isReverse>
-        <MarqueeText>예술 시장에 대한 의문점 의문점</MarqueeText>
-      </Marquee>
-      <Divider />
-      <Marquee duration={10}>
-        <MarqueeText>예술 시장에 대한 의문점 의문점</MarqueeText>
-      </Marquee>
-      <Divider />
-      <Marquee duration={10} isReverse>
-        <MarqueeText>예술 시장에 대한 의문점 의문점</MarqueeText>
-      </Marquee>
-      <Divider />
+      <MarqueeBox isScrolledDown={isScrolledDown}>
+        <Divider />
+        <Marquee duration={10}>
+          <MarqueeText>예술 시장은 성행하고 있지만 왜</MarqueeText>
+        </Marquee>
+        <Divider />
+        <Marquee duration={20} isReverse>
+          <MarqueeText>
+            아트 작품을 소유하고 컬렉팅하는 장벽은 여전히 높기만 하다
+          </MarqueeText>
+        </Marquee>
+        <Divider />
+        <Marquee duration={10}>
+          <MarqueeText>예술 시장에 대한 의문점 의문점</MarqueeText>
+        </Marquee>
+        <Divider />
+        <Marquee duration={10} isReverse>
+          <MarqueeText>예술 시장에 대한 의문점 의문점</MarqueeText>
+        </Marquee>
+        <Divider />
+        <Marquee duration={10}>
+          <MarqueeText>예술 시장에 대한 의문점 의문점</MarqueeText>
+        </Marquee>
+        <Divider />
+        <Marquee duration={10} isReverse>
+          <MarqueeText>예술 시장에 대한 의문점 의문점</MarqueeText>
+        </Marquee>
+        <Divider />
+        <Marquee duration={10} isReverse>
+          <MarqueeText>예술 시장에 대한 의문점 의문점</MarqueeText>
+        </Marquee>
+        <Divider />
+        <Marquee duration={10} isReverse>
+          <MarqueeText>예술 시장에 대한 의문점 의문점</MarqueeText>
+        </Marquee>
+        <Divider />
+        <Marquee duration={10} isReverse>
+          <MarqueeText>예술 시장에 대한 의문점 의문점</MarqueeText>
+        </Marquee>
+        <Divider />
+      </MarqueeBox>
+      <BottomBox isScrolledDown={isScrolledDown}>
+        <div ref={bottomScrollY.targetRef} />
+        <BottomTitleText>
+          NEW BID BY <br /> LOFA SEOUL
+        </BottomTitleText>
+        <Space height={10} />
+        <BottomBodyText>
+          아티스트의 제품을 구매하는 <br />
+          가장 오래된 방식의 새로운 해석 <br />
+          <br />
+          about 페이지 <br />
+          안내 안내 안내 안내{" "}
+        </BottomBodyText>
+      </BottomBox>
     </AboutPageBox>
   );
 }
