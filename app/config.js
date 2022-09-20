1f177e1a14f77db2894dea530a057ce29648d328
Taejung Kim <immcoc1@gmail.com>
2022-09-20 23:56:46 +0900

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,4 +1,2 @@
 // export const URL = "http://localhost:3000";
 export const URL = "https://newbid.netlify.app";
-export const KAKAO_JS_KEY = "810b82491c5d0c99501bf55ab85bce0f";
-export const KAKAO_REST_KEY = "13cb50f748fa0ea1bf651c4311112be7";
