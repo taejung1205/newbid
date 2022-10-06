a2a553906967e45eab4a02e829d796310f478c9c
Taejung Kim <immcoc1@gmail.com>
2022-10-06 11:51:20 +0900

    test: font

--- app/root.tsx
+++ app/root.tsx
@@ -52,6 +52,11 @@ export default function App() {
           {typeof document === "undefined" ? "__STYLES__" : null}
         </head>
         <body onLoad={() => window.scrollTo(0, 1)}>
+        <script
+            type="text/javascript"
+            src="https://z7z1yn5j64.execute-api.ap-northeast-2.amazonaws.com/V220930/api/fontstream/djs/?sid=gAAAAABjNoKB7aPtIJAn5jRMcYaz_Y-KZ4K-EOCGwhpW61tp_3yG3hoZRHqp-hAYyOzHi66fHUcsFs3gmIKAs3PK5K9MRVur0OOZR-1VNcmmUbFmkTom1C9u3WDjsk7A6-ZSpEGb_AdG6c_ksWhWQSzEFQrOS31--tX8_rDlpOm6sx-viCz_ZE7i5XDWuDSAbLocnrxVOACMAMVnNpmKqp79fYvMTixVBI2YqQWISV5ZO5u_wcqIlp_bSTMVr2u9-WNJutLtaBst"
+            charSet="utf-8"
+          />
           <Header />
           <Outlet />
           <Footer />
@@ -60,11 +65,6 @@ export default function App() {
           <script src="https://developers.kakao.com/sdk/js/kakao.js" />
           <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js" />
           <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js" />
-          <script
-            type="text/javascript"
-            src="https://z7z1yn5j64.execute-api.ap-northeast-2.amazonaws.com/V220930/api/fontstream/djs/?sid=gAAAAABjNoKB7aPtIJAn5jRMcYaz_Y-KZ4K-EOCGwhpW61tp_3yG3hoZRHqp-hAYyOzHi66fHUcsFs3gmIKAs3PK5K9MRVur0OOZR-1VNcmmUbFmkTom1C9u3WDjsk7A6-ZSpEGb_AdG6c_ksWhWQSzEFQrOS31--tX8_rDlpOm6sx-viCz_ZE7i5XDWuDSAbLocnrxVOACMAMVnNpmKqp79fYvMTixVBI2YqQWISV5ZO5u_wcqIlp_bSTMVr2u9-WNJutLtaBst"
-            charSet="utf-8"
-          />
           <script
             dangerouslySetInnerHTML={{
               __html: `window.ENV =  ${JSON.stringify(data.ENV)}`,
