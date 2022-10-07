d41543ea6b1d6874c844adbb2b451bede6ae5e2b
Taejung Kim <immcoc1@gmail.com>
2022-10-07 21:19:09 +0900

    test: instagram

--- app/root.tsx
+++ app/root.tsx
@@ -77,8 +77,32 @@ export default function App() {
           />
           <script
             dangerouslySetInnerHTML={{
-              __html: `if(navigator.userAgent.includes("Instagram")){
-              window.location.href = "https://newbid.netlify.app/test";
+              __html: `
+              var copytoclipboard = function(val){
+                var t = document.createElement("textarea");
+                document.body.appendChild(t);
+                t.value = val;
+                t.select();
+                document.execCommand('copy');
+                document.body.removeChild(t);
+              };
+              var inappbrowserout = function(){
+                copytoclipboard(window.location.href);
+                alert('URL주소가 복사되었습니다.\n\nSafari가 열리면 주소창을 길게 터치한 뒤, "붙여놓기 및 이동"를 누르면 정상적으로 이용하실 수 있습니다.');
+                location.href='x-web-search://?';
+              };
+              if(navigator.userAgent.includes("Instagram")){
+                //모바일대응뷰포트강제설정
+                var mobile = document.createElement('meta');
+                mobile.name = 'viewport';
+                mobile.content = "width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui";
+                document.getElementsByTagName('head')[0].appendChild(mobile);
+                //노토산스폰트강제설정
+                var fonts = document.createElement('link');
+                fonts.href = 'https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap';
+                document.getElementsByTagName('head')[0].appendChild(fonts);
+                document.body.innerHTML = "<style>body{margin:0;padding:0;font-family: 'Noto Sans KR', sans-serif;overflow: hidden;height: 100%;}</style><h2 style='padding-top:50px; text-align:center;font-family: 'Noto Sans KR', sans-serif;'>인앱브라우저 호환문제로 인해<br />Safari로 접속해야합니다.</h2><article style='text-align:center; font-size:17px; word-break:keep-all;color:#999;'>아래 버튼을 눌러 Safari를 실행해주세요<br />Safari가 열리면, 주소창을 길게 터치한 뒤,<br />'붙여놓기 및 이동'을 누르면<br />정상적으로 이용할 수 있습니다.<br /><br /><button onclick='inappbrowserout();' style='min-width:180px;margin-top:10px;height:54px;font-weight: 700;background-color:#31408E;color:#fff;border-radius: 4px;font-size:17px;border:0;'>Safari로 열기</button></article><img style='width:70%;margin:50px 15% 0 15%' src='https://tistory3.daumcdn.net/tistory/1893869/skin/images/inappbrowserout.jpeg' />";
+       
           }`,
             }}
           />
